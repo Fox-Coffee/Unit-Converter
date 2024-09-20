b70c44045030e58e696c6a70b75836188711333c
Eden Kamieniecka <f.kamieniecki.personal@gmail.com>
2024-09-20 15:21:44 +0200

    Created the LICENSE and README.md files, and modified the Program.cs file to improve the code

--- Program.cs
+++ Program.cs
@@ -1,50 +1,43 @@
-﻿internal class Program
+﻿/**
+    * Unit Converter - a program used to convert units
+    * Copyright (C) 2024  Eden kamieniecka
+    *
+    * This program is free software: you can redistribute it and/or modify
+    * it under the terms of the GNU General Public License as published by
+    * the Free Software Foundation, either version 3 of the License, or
+    * (at your option) any later version.
+    *
+    * This program is distributed in the hope that it will be useful,
+    * but WITHOUT ANY WARRANTY; without even the implied warranty of
+    * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    * GNU General Public License for more details.
+    *
+    * You should have received a copy of the GNU General Public License
+    * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+**/
+internal class Program
 {
     class DistanceUnit
     {
-        double toMilimeter;
-        double toCentimeter;
-        double toMeter;
-        double toKilometer;
-        double toInch;
-        double toFeet;
-        double toYard;
-        double toMile;
+        private readonly Dictionary<string, double> conversionFators;
 
         public DistanceUnit(double toMilimeter, double toCentimeter, double toMeter, double toKilometer, double toInch, double toFeet, double toYard, double toMile)
         {
-            this.toMilimeter = toMilimeter;
-            this.toCentimeter = toCentimeter;
-            this.toMeter = toMeter;
-            this.toKilometer = toKilometer;
-            this.toInch = toInch;
-            this.toFeet = toFeet;
-            this.toYard = toYard;
-            this.toMile = toMile;
+            conversionFators = new Dictionary<string, double>
+            {
+                { "mm", toMilimeter },
+                { "cm", toCentimeter },
+                { "m", toMeter },
+                { "km", toKilometer },
+                { "in", toInch },
+                { "ft", toFeet },
+                { "yd", toYard },
+                { "mi", toMile }
+            };
         }
         public double convert(double value, string unit)
         {
-            switch (unit)
-            {
-                case "mm":
-                    return value * toMilimeter;
-                case "cm":
-                    return value * toCentimeter;
-                case "m":
-                    return value * toMeter;
-                case "km":
-                    return value * toKilometer;
-                case "in":
-                    return value * toInch;
-                case "ft":
-                    return value * toFeet;
-                case "yd":
-                    return value * toYard;
-                case "mi":
-                    return value * toMile;
-                default:
-                    return 0;
-            }
+            return conversionFators.TryGetValue(unit, out double conversionFactor) ? value * conversionFactor : 0;
         }
     }
 
@@ -57,14 +50,10 @@
     static DistanceUnit Yards = new DistanceUnit(914.4, 91.44, 0.9144, 0.0009144, 36, 3, 1, 0.0005681818);
     static DistanceUnit Miles = new DistanceUnit(1609344, 160934.4, 1609.344, 1.609344, 63360, 5280, 1760, 1);
 
-
-
-
-    public static void Main(string[] args)
-    {
+    public static void ProgramMain(){
         Console.WriteLine("What unit do you want to convert from?");
         Console.WriteLine("mm, cm, m, km, in, ft, yd, mi");
-        string fromUnit = Console.ReadLine();
+        string? fromUnit = Console.ReadLine();
 
         Console.WriteLine("What is the value you want to convert?");
         if(!double.TryParse(Console.ReadLine(), out double value))
@@ -75,43 +64,68 @@
 
         Console.WriteLine("What unit do you want to convert to?");
         Console.WriteLine("mm, cm, m, km, in, ft, yd, mi");
-        string toUnit = Console.ReadLine();
+        string? toUnit = Console.ReadLine();
+        toUnit ??= "";
 
-        switch(fromUnit) {
-            case "mm":
-                Console.WriteLine(Milimeters.convert(value, toUnit));
-                break;
-            case "cm":
-                Console.WriteLine(Centimeters.convert(value, toUnit));
-                break;
-            case "m":
-                Console.WriteLine(Meters.convert(value, toUnit));
-                break;
-            case "km":
-                Console.WriteLine(Kilometers.convert(value, toUnit));
-                break;
-            case "in":
-                Console.WriteLine(Inches.convert(value, toUnit));
-                break;
-            case "ft":
-                Console.WriteLine(Feet.convert(value, toUnit));
-                break;
-            case "yd":
-                Console.WriteLine(Yards.convert(value, toUnit));
-                break;
-            case "mi":
-                Console.WriteLine(Miles.convert(value, toUnit));
-                break;
-            default:
-                Console.WriteLine("Invalid input. Please enter a valid unit.");
-                break;
+        DistanceUnit? unit = fromUnit switch
+        {
+            "mm" => Milimeters,
+            "cm" => Centimeters,
+            "m" => Meters,
+            "km" => Kilometers,
+            "in" => Inches,
+            "ft" => Feet,
+            "yd" => Yards,
+            "mi" => Miles,
+            _ => null
+        };
+
+        if (unit == null)
+        {
+            Console.WriteLine("Invalid unit");
+            return;
         }
+
+        double result = unit.convert(value, toUnit);
+        Console.WriteLine($"Result: {result} {toUnit}");
 
         Console.WriteLine("Do you want to convert another value? (y/n)");
-        string response = Console.ReadLine();
+        string? response = Console.ReadLine();
         if (response == "y")
         {
-            Main(args);
+            ProgramMain();
         }
+    }
+
+
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Unit Converter  Copyright (C) 2024  Eden Kamieniecka\nThis program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\nThis is free software, and you are welcome to redistribute it\nunder certain conditions; type `show c' for details.");
+        while (true)
+        {
+            Console.WriteLine("\n\nWhat would you like to do?\ns - start program\na - about the program\nc- display license details\ne-exit the program");
+            string? userInput = Console.ReadLine();
+
+            switch (userInput)
+            {
+                case "s":
+                    ProgramMain();
+                    break;
+                case "a":
+                    Console.WriteLine("This is a program used to convert units of measurment (ex. meters to yards). Currently the program allows to convert units of distance. The program runs in the command prompt or terminal.\nAuthor: Eden Kamieniecka\nVersion: \x1b[1mPREVIEW 0.0.0");
+                    break;
+                case "c":
+                    Console.WriteLine("This project is under the GPL-3 license, to learn more visit https://www.gnu.org/licenses/gpl-3.0.en.html.\nThis software is free to use and modify, but a modified version of the program has to be distributed under the same license.\nThe program's source code can be found on https://github.com/Fox-Coffee/unit-converter");
+                    Console.WriteLine("Developers that use the GNU GPL protect your rights with two steps:\n(1) assert copyright on the software, and \n(2) offer you this Licensegiving you legal permission to copy, distribute and/or modify it.");
+                    Console.WriteLine("Copyright © 2024, Eden Kamieniecka\nAll Rights Reserved");
+                    break;
+                case "e":
+                    return;
+                default:
+                    Console.WriteLine("Incorrect command");
+                    break;
+            }
+        }
+
     }
 }
---
+++ README.md
@@ -0,0 +1,16 @@
+# Unit converter
+This is a program used to convert units of measurment (ex. meters to yards). Currently the program allows to convert units of distance. The program runs in the command prompt or terminal.
+
+## Features
+The program currently allows for conversion of distance from one unit to another.
+
+## How to run?
+
+1. Download git
+2. Clone the repository using `git clone https://github.com/Fox-Coffee/unit-converter.git`
+3. Open the repository in an IDE of your choice (ex. Visual Studio, Rider) 
+4. Compile and run it
+
+## License
+
+This project is under the [GPL-3 license](https://www.gnu.org/licenses/gpl-3.0.en.html).
